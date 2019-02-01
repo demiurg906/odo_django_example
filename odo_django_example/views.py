58e4d03e0abd12b56f257735215617e5998f2a4d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2019-02-01 19:58:11 +0300

    Fix `get_question`

--- odo_django_example/views.py
+++ odo_django_example/views.py
@@ -17,13 +17,6 @@ def hello(request: HttpRequest, name: str = None):
 def get_question(request: HttpRequest, question_id: int):
     try:
         question = Question.objects.get(pk=question_id)
-        next_id = question_id + 1
-        previous_id = question_id - 1
-        if not Question.objects.filter(pk=next_id).exists():
-            next_id = None
-        if not Question.objects.filter(pk=previous_id).exists():
-            previous_id = None
-        Question.objects.filter()
-        return render(request, 'question.html', {'question': question, 'next_id': next_id, 'previous_id': previous_id})
+        return render(request, 'question.html', {'question': question})
     except Question.DoesNotExist:
         raise Http404(f'Question {question_id} does not exists')
